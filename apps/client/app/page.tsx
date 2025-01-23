eb222ffe7a01ac14eb6d4ca0732f1ed6617cf933
hwang <1201q@naver.com>
2025-01-23 13:39:38 +0900

    test: vercel 연결 테스트

--- apps/client/app/page.tsx
+++ apps/client/app/page.tsx
@@ -1,5 +1,22 @@
-"use client";
+export default async function Home() {
+  const data = await fetch(
+    "https://api.coingosu.live/upbit/candle/years?market=KRW-BTC&count=1"
+  )
+    .then((response) => {
+      if (response.ok) {
+        return response.json(); // 응답 본문을 JSON으로 파싱
+      } else {
+        throw new Error("Network response was not ok");
+      }
+    })
+    .then((jsonData) => {
+      console.log(jsonData); // 실제 데이터 출력
+      return jsonData;
+    })
+    .catch((error) => {
+      console.error("Fetch error:", error);
+    });
+  console.log(data); // 데이터 출력
 
-export default function Home() {
-  return <div>2</div>;
+  return <div>{data ? data[0].trade_price : "없음"}</div>;
 }
