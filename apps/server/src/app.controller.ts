dd96d2d6ba56386e785947ffe9931859a0b5b83f
hwang <1201q@naver.com>
2025-03-05 15:47:22 +0900

    fix: 배포 오류 수정

--- apps/server/src/app.controller.ts
+++ apps/server/src/app.controller.ts
@@ -4,6 +4,6 @@ import { Controller, Get } from "@nestjs/common";
 export class AppController {
   @Get()
   getHello(): string {
-    return `테스트 + ${process.env.TEST_ENV} 서버 `;
+    return `테스트 + ${process.env.TEST_ENV} `;
   }
 }
