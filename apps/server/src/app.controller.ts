3c720f998ffcfc577bda59993cb5659e29b30571
hwang <1201q@naver.com>
2025-03-05 16:18:20 +0900

    chore: env 따로 따로 분리

--- apps/server/src/app.controller.ts
+++ apps/server/src/app.controller.ts
@@ -4,6 +4,6 @@ import { Controller, Get } from "@nestjs/common";
 export class AppController {
   @Get()
   getHello(): string {
-    return `1테스트 + ${process.env.TEST_ENV} `;
+    return `2테스트 + ${process.env.TEST_ENV} `;
   }
 }
